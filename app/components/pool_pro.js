f97c384250871ba6a3570e61432ad518562aeb2e
Dave jan <devilvagrant@gmail.com>
2017-09-17 13:22:10 -0400

    update files
    update files

--- app/components/pool_pro.js
+++ app/components/pool_pro.js
@@ -4,6 +4,30 @@ export default class Aside extends React.Component {
     render() {
         return (
             <main id="content" class="main-content">
+               {/*
+                <div class="list-result">
+                    <h4 class="pool-pro-name">dealers.name</h4>
+                    <hr/>
+                    <h5 class="pool-pro-phone"><i class="fa fa-phone" aria-hidden="true"></i><span class="mobile-call">Tap to call </span>dealers.phone1</h5>
+                    <p><em>Can't talk now?, Click below to send email.</em></p>
+                    <a class="pro-email" href="dealers.email"><i class="fa fa-envelope" aria-hidden="true"></i>Contact This Pro</a>
+                    <ol>
+                        <li><strong>Business Hours</strong></li>
+                        <li>Weekdays: weekHours</li>
+                        <li>Saturdays: weekHours.sat</li>
+                        <li>Sundays: weekHours.sun</li>
+                    </ol>
+                    <div class="list-footer">
+                        <div class="left-type-service">
+                            <p><i class="fa fa-star" aria-hidden="true"></i> Installation Pro</p>
+                            <p><i class="fa fa-home" aria-hidden="true"></i> Residential Pro</p>
+                        </div>
+                        <div class="right-type-service">
+                            <p><i class="fa fa-users" aria-hidden="true"></i> Service Pro</p>
+                        </div>
+                    </div>
+                </div>
+               */}
                 <div class="list-result">
                     <h4 class="pool-pro-name">Aqua Experts</h4>
                     <hr/>
